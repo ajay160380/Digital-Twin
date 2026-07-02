de5ef8dadf2cf2077484b367fb0cf70311354f23
ajay160380 <ajaykumar160380@gmail.com>
2026-07-03 00:17:01 +0530

    Fix HF Spaces refused to connect issue by removing XFrameOptionsMiddleware

--- digital_twin_pro/settings.py
+++ digital_twin_pro/settings.py
@@ -41,7 +41,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'digital_twin_pro.urls'
